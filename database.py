b657ae82c2f8877bc94629c44c8cffb98035d6df
Kevin Jiang <kevin.jkfeng@gmail.com>
2019-04-16 22:49:10 +0800

    add new function - add_tickers

--- database.py
+++ database.py
@@ -4,49 +4,68 @@ from pybbg import pybbg_k as pybbg
 from datetime import datetime,date,time
 
 class Database_hdf5(object):
-
+    '''
+    This class is use to download data from bloomberg using pybbg package and store data in HDF5 
+    '''
     def __init__(self):
+        '''initialize configurations'''
 
         self.bbg = pybbg.Pybbg()
         self.bbg.service_refData()
         self.path = r'C:\Users\Kevin\OneDrive - The Chinese University of Hong Kong\Documents\Quant Trading\Database\hdf5_db'
-        self.table_daily ='data_daily.h5'
-    def download_trading_data(self,tickers,start_date,end_date,fields):
-        self.tickers = tickers
-        self.start_date = start_date.strftime('%Y%m%d')
-        self.end_date = end_date.strftime('%Y%m%d')
-        self.fields = fields
-        price = self.bbg.bdh(self.tickers, self.fields, self.start_date, self.end_date, periodselection='DAILY')
-        price = price.stack(level=1)
-        price.reset_index(inplace=True)
-        price.columns.values[0]='date'
-        return price
-    def write_trading_data(self,trading_data,path,name,group):
-        store = pd.HDFStore(os.path.join(path,name))
+        self.file_name ='price.h5'
+        self.group_name ='HK'
+        self.tickers = ['5 HK Equity','1 HK Equity']
+        self.price_fields = ['PX_OPEN', 'PX_HIGH', 'PX_LOW', 'PX_LAST', 'PX_VOLUME']
+        self.start_date=date(2018,1,1)
+        self.end_date=date(2019,1,1)
         
-        tables = dict(list(trading_data.groupby(['field']))) #store into tables by fields
-        for key,values in tables.items():
-            df = values.drop(columns=['field'],axis=1)
-            df.set_index('date',inplace=True)
-            tables[key]=df
-            store.put(os.path.join(group,key),tables[key],data_columns=True)
-        store.close()
-    def read_trading_data(self,path,file_name,group_name,fields):
+    def download_daily_data(self,tickers,start_date,end_date,fields):
+
+        data = self.bbg.bdh(tickers,fields,start_date,end_date, periodselection='DAILY')
+        data.index.name='date'
+        data = data.stack(level=0)
+        return data
+    def write_daily_data(self,daily_data,path,file_name,group_name):
+        daily_data = daily_data.unstack().stack(level=0)
+        with pd.HDFStore(os.path.join(path,file_name),mode='a') as store:
+        
+            tables = dict(list(daily_data.groupby(['field']))) #store into tables by fields
+            for key,values in tables.items():
+                df = values.droplevel(level=1)
+                #tables[key]=df
+                store.put('/'+group_name+'/'+key,df,data_columns=True)
+                #store.put(os.path.join(group_name,key),tables[key],data_columns=True)
+            
+    def read_daily_data(self,path,file_name,group_name,fields):
         data ={}
-        for fd in fields:
-            data[fd] = pd.read_hdf(os.path.join(path,file_name),os.path.join(group_name,fd))
+        #store = pd.HDFStore(os.path.join(path,file_name))
+        with pd.HDFStore(os.path.join(path,file_name),mode='r') as store:
+            for fd in fields:
+                data[fd] = store.select('/'+group_name+'/'+fd)
         df = pd.concat(data,axis=1)
+        #store.close()
         return df.stack(level=1)
-def updateDB(db):
+    def add_tickers(self,new_tickers,path,file_name,group_name,fields):
+        '''
+        add new tickers with field data and update the specified table in HDF5
+        '''
+        new_data = self.download_daily_data(new_tickers,self.start_date,self.end_date,fields)
+        old_data = self.read_daily_data(path,file_name,group_name,fields)
+        merged_data = pd.concat([old_data.unstack().stack(level=0),new_data.unstack().stack(level=0)],axis=1)
+        merged_data = merged_data.unstack().stack(level=0)
+        merged_data.columns.name = 'field'
+        self.write_daily_data(merged_data,path,file_name,group_name)
+        
+def updatePrice(db):
     
-    tickers = ['5 HK Equity','1 HK Equity']
-    trading_fields = ['PX_OPEN', 'PX_HIGH', 'PX_LOW', 'PX_LAST', 'PX_VOLUME']
-    trading_data = db.download_trading_data(tickers,date(2018,1,1),date(2019,1,1),trading_fields)
-    db.write_trading_data(trading_data,db.path,db.table_daily,'HK')
+    price_data = db.download_daily_data(db.tickers,db.start_date,db.end_date,db.price_fields)
+    db.write_daily_data(price_data,db.path,db.file_name,db.group_name)
     
 if __name__ == "__main__":
     db = Database_hdf5()
-    updateDB(db)
+    updatePrice(db)
+    db.add_tickers(['700 HK Equity'],db.path,db.file_name,db.group_name,['PX_OPEN', 'PX_HIGH'])
 
 
 
